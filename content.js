ab40671055a266c21f7faedb9c9f08e397c18f8d
Ayush Singh <ayush.singh.csbs22@ggits.net>
2025-05-17 00:48:17 +0530

    fix: Fix Content.js file

--- content.js
+++ content.js
@@ -1,17 +1,15 @@
-// works on web pages Dom //
+function getArticleText() {
+  const article = document.querySelector("article");
+  if (article) return article.innerText;
 
-function getArticle(){
-    const article = document.querrySelector("article");
-    if(article) return article.innerText;
-
-    const paragraphs = Arrays.from(document.querySelectorAll("p"));
-    return paragraphs.map((p)=> p.innertext).join("/n");
-
+  // fallback
+  const paragraphs = Array.from(document.querySelectorAll("p"));
+  return paragraphs.map((p) => p.innerText).join("\n");
 }
 
-chrome.runtime.onMessage.addListener((req, _sender, sendResponse) => {
-    if(req.type == "GET_ARTICLE_TEXT"){
-        const text = getArticleText();
-        sendResponse({text});
-    }
+chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
+  if (req.type === "GET_ARTICLE_TEXT") {
+    const text = getArticleText();
+    sendResponse({ text });
+  }
 });
